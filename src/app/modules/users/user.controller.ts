af94132fa169c62da458fd62db14321a7bac82d2
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2023-06-06 10:04:28 +0600

    [Done] Global error handleing done

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -2,6 +2,10 @@ import { RequestHandler } from 'express'
 import { UserService } from './user.service'
 
 const createUser: RequestHandler = async (req, res, next) => {
+  //  req-validation
+  // body -> Object
+  // data -> object
+
   try {
     const { user } = req.body
     const result = await UserService.createUser(user)
